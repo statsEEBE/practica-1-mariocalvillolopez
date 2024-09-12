815b73ad00b44901fe6af34d4f7557a988986d64
mario.calvillo <mario.calvillo@auleseebe.upc.edu>
2024-09-12 11:51:31 +0200

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,36 @@
-#Codigo para problema 1
+#Codigo para problema 1mmm
 
-plot(1:5, col="red", pch=16)
+mis_dades <- mtcars
+
+mis_dades$qsec
+mean(mis_dades$qsec)
+
+hist(mis_dades$qsec)
+table(cut(mis_dades$qsec,9))/32
+
+freq <- table(cut(mis_dades$qsec,9))
+tabla <- data.frame(ni=freq,
+                    fi=freq/32,
+                    Ni=cumsum(freq),
+                    Fi=cumsum(freq)/32)
+
+tabla
+mis_dades$drat
+hist(mis_dades$drat)
+
+median(mis_dades$drat)
+
+sort(mis_dades$drat)
+
+quantile(mis_dades$drat)
+
+quantile(mis_dades$mpg,0.18)
+
+sort(mis_dades$cyl)
+
+IQR(mis_dades$cyl)
+
+quantile (mis_dades$cyl)
+
+sd(mis_dades$cyl)
+var(mis_dades$cyl)
